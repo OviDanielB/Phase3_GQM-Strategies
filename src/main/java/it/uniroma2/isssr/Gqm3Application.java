9c1cfc50ec697355348301a7f945d97524f18f8e
Ovidiu Daniel Barba <ovi.daniel.b@gmail.com>
2017-07-13 15:19:17 +0200

    pom modified for war packaging

--- src/main/java/it/uniroma2/isssr/Gqm3Application.java
+++ src/main/java/it/uniroma2/isssr/Gqm3Application.java
@@ -2,13 +2,20 @@ package it.uniroma2.isssr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * @author emanuele
  */
 
 @SpringBootApplication
-public class Gqm3Application {
+public class Gqm3Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Gqm3Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Gqm3Application.class, args);
